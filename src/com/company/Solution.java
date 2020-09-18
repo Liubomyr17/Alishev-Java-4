b5e0c7b910ba9525193c387d29301ac5c4b87c42
Liubomyr Izdepskyi <41821541+Liubomyr17@users.noreply.github.com>
2020-09-18 23:47:32 +0300

    Add files via upload

---
+++ src/com/company/Solution.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import javax.xml.bind.SchemaOutputResolver;
+import java.io.Serializable;
+
+public class Solution {
+    public static void main(String[] args) {
+       int value = 0;
+       while(value<5) {
+         System.out.println("Hello" + value);
+         value = value + 1;
+    }
+    }
+}
